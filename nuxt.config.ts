49d1e0e52d6d52db744362d662c8d7cc9306b256
Daniel Kelly <me@danielkelly.io>
2026-01-12 07:52:35 -0600

    chore: upgrade to v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  compatibilityDate: "2026-01-12",
   modules: ["@nuxt/ui"],
   colorMode: {
     preference: "dark",
