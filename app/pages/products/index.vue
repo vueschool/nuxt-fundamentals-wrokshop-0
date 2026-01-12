c1532d3dc5ca146fa5692785e29550ea88145824
Daniel Kelly <me@danielkelly.io>
2026-01-12 13:54:02 -0600

    feat: cached data bonus challenge

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -7,6 +7,9 @@ const { data: products } = await useFetch("/api/products", {
 
       return restOfProperties;
     }),
+  getCachedData(key, nuxtApp) {
+    return nuxtApp.payload.data[key];
+  },
 });
 </script>
 
